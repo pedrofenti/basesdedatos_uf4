57f9e2dcef37af5589afef7d1e4601e23bb4abf9
pedrofenti <pedro.fernandez@enti.cat>
2021-04-23 11:28:38 +0200

    leyendo los datos de mongodb

--- index.js
+++ index.js
@@ -3,33 +3,67 @@
 const http = require("http");
 const node_static = require("node-static");
 
-const mongo = require("mongodb").MongoClient;
+const mongo = require("mongodb").MongoClient; //para conectarse a mongo
 
-let server_url = "mongodb://localhost:27017";
+let server_url = "mongodb://localhost:27017"; //servidor de mongodb
+
+let chat_db; //la definimos global para acceder desde fuera
 
 mongo.connect(server_url, (error, server) => {
 	if (error){
 		console.log("Error en la conexion a MongoDB");
-		throw error; //return para errores, lanza un error
+		throw error; //throw es un return para errores, lanza un error
 		}
 		
 		console.log("Dentro de MongoDB");
 	
-		let chat_db = server.db("amongmeme");
-
-		chat_db.collection("chat").find({});
-
+		chat_db = server.db("amongmeme"); //conexion a la base de datos
 });
+
+function sendChat(response){
+
+	let cursor = chat_db.collection("chat").find({}); //puntero a los datos
+
+	let data = [];
+
+	//es un for.each pero propio de los cursores
+	cursor.each( (error, doc) => {
+		if (error){
+			console.log("Error al ller el documento");
+			throw error;
+		}
+		
+		data.push(doc);
+
+	//CUANDO SEA EL ULTIMO CASO HACEMOS ESTO
+	//transformar el array 'data' a string y enviarselo al cliente
+	//response.write(data);
+	//response.end();
+
+	});
+	console.log(data);
+}
+
 
 console.log("Inicializando servidor chat");
 
-/*
-let public_files = new node_static.Server("pub");
+
+let public_files = new node_static.Server("pub"); //directorio de nuestro sismeta y por defecto de nuestro servidor web
 
 http.createServer( (request, response) => {
+	
+	console.log("Archivo "+request.url);
+	
+	if (request.url == "/chat"){
+		console.log("Nos piden el chat de mongodb");
+		response.writeHead(200, {'Content-Type':'text/plain'}); //error 200 es 'todo encontrado' es decir esta bien
+		//response.write("<p>Ahora viene el chat</p>");
+		//response.end();
 
-	console.log("Archivo"+request.url);
-
-	public_files.serve(request, response);
+		sendChat(response);
+	}
+	else{
+		public_files.serve(request, response);
+	}
+                 
 }).listen(8080);
-*/
