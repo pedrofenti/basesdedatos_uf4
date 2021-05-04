10db1f49bc2ebfeddcb933eedf3edc3632e15ff0
pedrofenti <pedro.fernandez@enti.cat>
2021-05-04 13:13:40 +0200

    POR FIN FUNCIONA

--- index.js
+++ index.js
@@ -2,68 +2,86 @@
 
 const http = require("http");
 const node_static = require("node-static");
+ 
+//Para conectarse a mongo
+const mongo = require("mongodb").MongoClient;
 
-const mongo = require("mongodb").MongoClient; //para conectarse a mongo
+//Servidor de mongodb
+let server_url = "mongodb://localhost:27017";
 
-let server_url = "mongodb://localhost:27017"; //servidor de mongodb
+//la definimos global para acceder desde fuera
+let chat_db;
 
-let chat_db; //la definimos global para acceder desde fuera
+mongo.connect(server_url, (err, server) => {
+	if (err){
+		console.log("Error en la conexión a MongoDB");
+		//throw es un return para errores, lanza un error
+		throw err;	
+	}
 
-mongo.connect(server_url, (error, server) => {
-	if (error){
-		console.log("Error en la conexion a MongoDB");
-		throw error; //throw es un return para errores, lanza un error
-		}
-		
-		console.log("Dentro de MongoDB");
+	console.log("Dentro de MongoDB");
+
+	//conexion a la base de datos
+	chat_db = server.db("amongmeme");
 	
-		chat_db = server.db("amongmeme"); //conexion a la base de datos
 });
-
-function sendChat(response){
-
-	let cursor = chat_db.collection("chat").find({}); //puntero a los datos
-
-	let data = [];
-
-	//es un for.each pero propio de los cursores
-	cursor.each( (error, doc) => {
-		if (error){
-			console.log("Error al ller el documento");
-			throw error;
-		}
-		
-		data.push(doc);
-
-	//CUANDO SEA EL ULTIMO CASO HACEMOS ESTO
-	//transformar el array 'data' a string y enviarselo al cliente
-	//response.write(data);
-	//response.end();
-
-	});
-	console.log(data);
-}
 
 
 console.log("Inicializando servidor chat");
 
-
-let public_files = new node_static.Server("pub"); //directorio de nuestro sismeta y por defecto de nuestro servidor web
+//directorio de nuestro sismeta y por defecto de nuestro servidor web
+let public_files = new node_static.Server("pub");
 
 http.createServer( (request, response) => {
-	
-	console.log("Archivo "+request.url);
-	
 	if (request.url == "/chat"){
-		console.log("Nos piden el chat de mongodb");
-		response.writeHead(200, {'Content-Type':'text/plain'}); //error 200 es 'todo encontrado' es decir esta bien
-		//response.write("<p>Ahora viene el chat</p>");
-		//response.end();
+		//console.log("Entrando en el chat");
+		
+		//puntero a los datos
+		let cursor = chat_db.collection("chat").find({});
 
-		sendChat(response);
+		cursor.toArray().then( (data) => {
+			//console.log(data);
+
+			//error 200 es 'todo encontrado' es decir esta bien
+			response.writeHead(200, {'Content-Type': 'text/plain'});			
+			
+			response.write( JSON.stringify(data) );
+
+			response.end();
+		});
+
+		return;
 	}
-	else{
-		public_files.serve(request, response);
+
+	if (request.url == "/submit"){
+		console.log("Envío de datos");
+
+		let body = [];
+
+		//chunk son los bloques de datos
+		request.on('data', (chunk) => {
+
+			body.push(chunk);
+
+		}).on('end', () => {
+			
+			//convierte a objetos 
+			let chat_data = JSON.parse(Buffer.concat(body).toString());
+
+			//insertamos el valor en la base de datos
+			chat_db.collection("chat").insertOne({
+				user: chat_data.chat_user,
+				msg: chat_data.chat_msg,
+				date: Date.now()
+			}); 
+
+		});
+
+		response.end();
+
+		return;
 	}
-                 
+
+	public_files.serve(request, response);
+
 }).listen(8080);
